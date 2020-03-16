ed702c78ac5cbb51687b51fd95094f1d4ce71e62
Bin-Guan <binguan2@gmail.com>
2020-03-16 09:22:00 -0400

    bcl2fastq v1

--- trimming_fastq.sh
+++ trimming_fastq.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 module load fastxtoolkit
-trimmed-length = $1
-for file in *.fastq.gz;
+#trimmed-length = $1
+mkdir -p fastq$1
+for file in LGE_CnR_4_S4_*.fastq.gz;
  do 
- gunzip -c $file | fastx_trimmer -l $1 -i - | gzip - > $1-$file; done
+ gunzip -c $file | fastx_trimmer -l $1 -i - | gzip - > fastq$1/$file; done
  
